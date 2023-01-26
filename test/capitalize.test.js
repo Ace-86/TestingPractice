6d165ee4c3d8f2b1f157aa4d04472db7ddf10a7e
Ace <kronicrespawn@gmail.com>
2023-01-25 21:51:13 -0600

    2 test added; broke name test

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -1,14 +1,12 @@
 const capitalizeLetter = require("./capitalize");
-// const sum = require('./sum');
 
-// describe("Capitalize", () => {
+
     test("Name", () => {
         expect(capitalizeLetter("foo")).toBe("Foo");
     });
-    // test("Is Undefined", () => {
-    //     expect(capitalizeLetter()).toBe("");
-    // });
-    // test("Is Null", () => {
-    //     expect(capitalizeLetter(null)).toBe("");
-    // });
-// });
+    test("Is Undefined", () => {
+        expect(capitalizeLetter()).toBe("");
+    }); 
+    test("Is Null", () => {
+        expect(capitalizeLetter(null)).toBe("");
+    });
