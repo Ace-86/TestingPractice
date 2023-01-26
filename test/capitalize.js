479af52d8fb76627348a374a35bc4d27c32fbee9
Ace <kronicrespawn@gmail.com>
2023-01-25 21:50:08 -0600

    if statement for undefined

--- test/capitalize.js
+++ test/capitalize.js
@@ -1,7 +1,10 @@
 function capitalizeLetter(string) {
+    if (string = '') {
+        return ""
+    } else {
     return string.charAt(0).toUpperCase() + string.slice(1)
+};
 }
-
-console.log(capitalizeLetter('foo'));
+// console.log(capitalizeLetter('foo'));
 
 module.exports = capitalizeLetter;
