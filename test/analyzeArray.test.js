4a57a860983788fb8220687fcbcd54124218a4a4
Ace <kronicrespawn@gmail.com>
2023-01-27 09:49:11 -0600

    4 test added

---
+++ test/analyzeArray.test.js
@@ -0,0 +1,16 @@
+
+
+test('find the average', () => {
+    expect(caesar([1,8,3,4,2,6]).toBe(4));
+  });
+  test('find the minimum', () => {
+    expect(caesar([1,8,3,4,2,6]).toBe(1));
+  });
+  test('find the max', () => {
+    expect(caesar([1,8,3,4,2,6]).toBe(8));
+  });
+  test('find the length', () => {
+    expect(caesar([1,8,3,4,2,6]).toBe(6));
+  });
+  
+  
