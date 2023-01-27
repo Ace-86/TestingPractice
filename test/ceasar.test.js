d971a2bd2ee42a38b22a1ab6bb6cca39132b25a4
Ace <kronicrespawn@gmail.com>
2023-01-27 09:38:45 -0600

    2 test added/passed

--- test/ceasar.test.js
+++ test/ceasar.test.js
@@ -3,4 +3,10 @@ const { caesar } = require("./ceasar");
 test('Ceasar cipher', () => {
   expect(caesar("adam", 1)).toMatch("bebn");
 });
+test('word is undefined', () => {
+  expect(caesar("", 1)).toMatch("");
+});
+test('no shift in cipher', () => {
+  expect(caesar("adam", 0)).toMatch("adam");
+});
 
