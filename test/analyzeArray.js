b25ad9e2eec012c3bb9de9b557fa15e2ab6fd685
Ace <kronicrespawn@gmail.com>
2023-01-28 16:53:54 -0600

    findMin function added

--- test/analyzeArray.js
+++ test/analyzeArray.js
@@ -1,5 +1,14 @@
 function getAverage(values) {
     return values.reduce((a, b) => a + b, 0) / values.length;
 }
-console.log(getAverage([1,2,3,4,5,6,7]))
-//reducer() 
+// console.log(getAverage([1,2,3,4,5,6,7]))
+
+
+function findMin(value) {
+    if (toString.call(value) !== "[object Array]")
+    return false;
+    return Math.min.apply(null, value);
+}
+
+console.log(findMin([1, 2, 3, 4, 5, 6, 7]));
+// findMin();
