aae5afcb0a0606ccb498e05ed3d0abef23287a3f
Ace <kronicrespawn@gmail.com>
2023-01-28 17:43:19 -0600

    functions exported

--- test/analyzeArray.js
+++ test/analyzeArray.js
@@ -1,28 +1,30 @@
-function getAverage(values) {
-    return values.reduce((a, b) => a + b, 0) / values.length;
+const getAverage = (value) => {
+    return value.reduce((a, b) => a + b, 0) / value.length;
 }
 // console.log(getAverage([1,2,3,4,5,6,7]))
 
 
-function findMin(value) {
+const findMin = (value) => {
     if (toString.call(value) !== "[object Array]") //verifies if value is a type of array
     return false;
     return Math.min.apply(null, value);
 }
 
-console.log(findMin([1, 2, 3, 4, 5, 6, 7]));
+// console.log(findMin([1, 2, 3, 4, 5, 6, 7]));
 // findMin();
 
-function findMax(value) {
+const findMax = (value) => {
     return Math.max.apply(null, value);
 }
 
-console.log(findMax([1, 2,3,4,5,6,7]))
+// console.log(findMax([1, 2,3,4,5,6,7]))
 
-function findLength(value) {
+const findLength = (value) => {
     if(toString.call(value) !== "[object Array]")
     return false
     return value.length
 }
 
-console.log(findLength([1,2,3,4,5,6,7]))
+// console.log(findLength([1,2,3,4,5,6,7]))
+
+module.exports = {getAverage, findMin, findMax, findLength}
