a9ea54fb74682f80540702cce72982f46904858a
Ace <kronicrespawn@gmail.com>
2023-01-29 18:54:52 -0600

    uppercase test added

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -10,7 +10,9 @@ const {capitalizeLetter} = require("./capitalize");
     test("Is Null", () => {
         expect(capitalizeLetter(null)).toBe("");
     });
-
+    test("All uppercase", () => {
+        expect(capitalizeLetter("FOO")).toBe("Foo")
+    })
     //to-do: add test for all letters capitalized
     //add test for numbers and characters (eliminating and creating new string)
     //add test for spaces in words
