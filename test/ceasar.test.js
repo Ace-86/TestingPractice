7a578b1c8bfd9cedcab28575fa7c5394473d85c7
Ace <kronicrespawn@gmail.com>
2023-01-29 19:03:41 -0600

    number check test added

--- test/ceasar.test.js
+++ test/ceasar.test.js
@@ -10,8 +10,12 @@ test('no shift in cipher', () => {
   expect(caesar("adam", 0)).toMatch("adam");
 });
 test('symbols fail', () => {
-  expect(caesar("$%$@", 1)).toMatch("cannot use symbols")
+  expect(caesar("$%$@", 1)).toMatch("cannot contain symbols")
 })
+test('numbers fail', () => {
+  expect(caesar("2absad2")).toMatch("cannot contain numbers")
+})
+
 //to- do: test non-letters
 //test capitalization\
 //test mixed lower/uppercase
